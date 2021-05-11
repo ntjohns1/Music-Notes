0f8015a4719aac6bd03fcef8cbde627e5bb142b7
Hannah Lee <013hannah.a.lee@gmail.com>
2021-05-10 19:57:26 -0500

    fixed path api route to comment api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const studentRoutes = require('./student-routes');
-const commentRoutes = require('./comment-routes');
+const commentRoutes = require('./commentRoutes');
 
 router.use('/user', userRoutes);
 router.use('/student', studentRoutes);
