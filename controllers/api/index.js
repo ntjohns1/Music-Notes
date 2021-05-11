ca97c4cd41e83ff14fabced4af40a92226b98f45
Hannah Lee <013hannah.a.lee@gmail.com>
2021-05-10 19:33:21 -0500

    updated api routes with student-route. App will now get student information

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const studentRoutes = require('./student-routes');
 
 router.use('/user', userRoutes);
+router.use('/student', studentRoutes);
 
 module.exports = router;
