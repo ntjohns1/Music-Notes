f69ac32b1e40b07c599e71f8bc3067753657a1de
ntjohns1 <nelsontjohns@gmail.com>
2021-05-13 03:41:00 -0400

    tworking on populating comment history for selected student

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,17 +3,17 @@ const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-    if(req.session.logged_in) {
-        res.render('portal')
-    } else{
-        res.render('homepage')
+  if (req.session.logged_in) {
+    res.render('portal')
+  } else {
+    res.render('homepage')
 
-    }
+  }
 })
 
 router.get('/login', async (req, res) => {
   if (req.session.logged_in) {
-    
+
     res.render('portal')
     return;
   }
@@ -22,21 +22,29 @@ router.get('/login', async (req, res) => {
 });
 
 router.get('/signup', async (req, res) => {
-    if (req.session.logged_in) {
-      res.render('portal')
-      return;
-    }
-  
-    res.render('signup');
-  });
+  if (req.session.logged_in) {
+    res.render('portal')
+    return;
+  }
 
-  router.get('/logout', async (req, res) => {
-    if (req.session.logged_in) {
-      res.render('portal')
-      return;
-    }
-  
-    res.render('homepage');
+  res.render('signup');
+});
+
+router.get('/logout', async (req, res) => {
+  if (req.session.logged_in) {
+    res.render('portal')
+    return;
+  }
+
+  res.render('homepage');
+});
+
+router.get('/comment', async (req, res) => {
+  const userData = await User.findAll().catch((err) => {
+    res.json(err);
   });
+  const users = userData.map((user) => user.get({ plain: true }));
+  res.render('comment', { users });
+});
 
 module.exports = router;
--- public/js/comment.js
+++ public/js/comment.js
@@ -1,3 +1,26 @@
+// **user hits "go" app shows add comment section and hides dropdown
+// add submit button under text area that calls addComment, format inputs to match 
+// **function to hide/show appropriate page content
+// page script needs to fetch the comments for that student and populate the UI with them (id=commentHistory)
+// add event listeners to edit and delete buttons on comment history
+const { User, Comment } = require('../../models');
+const { findAll } = require('../../models/User');
+
+const selectStudent = async (event) => {
+    event.preventDefault();
+    const student = document.querySelector('#userSelect').value.trim();
+    document.querySelector('#userSelect').setAttribute('hidden', true);
+    document.querySelector('#manageComments').removeAttribute('hidden');
+    const  commentData = await Comment.findAll().catch((err) => {
+        res.json(err);
+      });
+      const comments = commentData.filter(comments => commentData.student_id === ))
+
+
+    // fetch comments for student 
+    // 
+}
+
 const viewComments = async (event) => {
     event.preventDefault();
     //select a student or findbyPK() and grab all the comments for that student along with the associated data
@@ -10,24 +33,27 @@ const addComment = async (event) => {
     const date = document.querySelector('#date-comment').value.trim();
     const content = document.querySelector('#content-comment').value.trim();
     const student_id = document.querySelector('#student_id-comment').value.trim();
-    const teacher_id = document.querySelector('#email-signup').value.trim();
+    const teacher_id = document.querySelector('#teacher_id-comment').value.trim();
 
 
     if (date && content && student_id && teacher_id) {
-        console.log(date, content, student_id, teacher_id )
-        const response = await fetch('/api/user/signup', {
+        console.log(date, content, student_id, teacher_id)
+        const response = await fetch('/api/comment', {
             method: 'POST',
             body: JSON.stringify({ date, content, student_id, teacher_id }),
             headers: { 'Content-Type': 'application/json' },
         })
-        if(response.ok) {
+        if (response.ok) {
             document.location.replace('/');
         } else {
-            alert('Failed to sign up')// change to a modal
+            alert('Failed to add comment')// change to a modal
         }
     }
 }
 
 document
-  .querySelector('.comment-form')
-  .addEventListener('submit', addComment);
+    .querySelector('.comment-form')
+    .addEventListener('submit', addComment);
+document
+    .querySelector('#userSelectBtn')
+    .addEventListener('submit', selectStudent);
