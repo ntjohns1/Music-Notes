e2a95768ac51fd4fbf3722711fa150d5bc5e2e8e
Hannah Lee <013hannah.a.lee@gmail.com>
2021-05-13 19:08:48 -0500

    fixed api routes, worked on events routes and event calendar code

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes.js');
-const portalRoutes = require('./portalRoutes.js')
+const portalRoutes = require('./portalRoutes.js');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-router.use('/portal', portalRoutes)
+router.use('/portal', portalRoutes);
 
-module.exports = router
+module.exports = router;
