5003ab96311e66ff3e0ff10954a4003e3f79126a
Suejin Kim <kim.suejin.20@gmail.com>
2021-05-13 20:18:49 -0400

    console logged picked date using splits

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes.js');
-const portalRoutes = require('./portalRoutes.js')
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
--- public/js/schedule.js
+++ public/js/schedule.js
@@ -3,19 +3,22 @@ const createEvent = async (event) => {
 
     //const userDate = document.querySelector('#date-schedule').value.trim();
     const pickedDate = document.querySelector('#datepicker').value
-    const userStartTime = document.querySelector('#startTime-scheduleLesson').value;
+    //const userStartTime = document.querySelector('#startTime-scheduleLesson').value;
     //const userEndTime = document.querySelector('#endTime-scheduleLesson').value.trim();
-    const studentName = document.querySelector('#studentName-scheduleLesson').value
-    const studentId = document.querySelector('#studentId-scheduleLesson').value;
-    const teacherId = document.querySelector('#teacherId-scheduleLesson').value;
+    //const studentName = document.querySelector('#studentName-scheduleLesson').value
+    //const studentId = document.querySelector('#studentId-scheduleLesson').value;
+    //const teacherId = document.querySelector('#teacherId-scheduleLesson').value;
     
-    console.log(pickedDate && userStartTime)
-
-    if (pickedDate && userStartTime) {
-        console.log(pickedDate && userStartTime)
-    } else {
-        alert('not working')
-    }
+    console.log(pickedDate)
+    const month = pickedDate.split("/")[0]
+    const day = pickedDate.split("/")[1]
+    const year = pickedDate.split("/")[2]
+    console.log ('month:'+ month + ' day:' + day + ' year:' + year)
+    // if (pickedDate && userStartTime) {
+    //     console.log(pickedDate && userStartTime)
+    // } else {
+    //     alert('not working')
+    // }
 }
 
 document
